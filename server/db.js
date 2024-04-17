1f8c45d769b7882fe880f88d44e817972a68eaa6
Abdullah Ehsan <abdullahehsan4242@gmail.com>
2024-04-18 03:51:11 +0500

    mongo env

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose'
-const mongoConnectionURL = process.env.MONGO_URL?? "mongodb+srv://dev1:kalimat123@kalimat.zg0uxv1.mongodb.net/?retryWrites=true&w=majority"
+const mongoConnectionURL = process.env.MONGO_URL
 
 export const mongo = await mongoose.connect(mongoConnectionURL)
 console.log("mongo connected")
