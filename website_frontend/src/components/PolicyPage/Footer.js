849ad19179d9b60c09374de31a33373be3f908fb
Salar-Amir <saldamme13@gmail.com>
2024-05-14 00:23:14 +0300

    Terms page added new

--- website_frontend/src/components/PolicyPage/Footer.js
+++ website_frontend/src/components/PolicyPage/Footer.js
@@ -7,13 +7,16 @@ const Footer = () => {
   const policyClick = () => {
     navigate("/policy");
   }
+  const termsClick = () => {
+    navigate("/termsandconditions");
+  }
   return (
     <div className="w-full bg-custom-dark-blue text-white">
       <div className="flex mx-12 justify-between items-center py-3">
         <div>© 2024 KalimatKeywords All rights reserved.</div>
         <div className="flex gap-4">
           <div onClick={policyClick}>الخصوصية</div>
-          <div >الشروط والأحكام</div>
+          <div onClick={termsClick}>الشروط والأحكام</div>
         </div>
       </div>
     </div>
